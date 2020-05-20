4f867c74b6142e9340b9b2820a3e9325877e38c2
Kevin Nguy <nguy@macbook.local>
2020-05-20 16:53:30 -0700

    Implement app navigation in App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,71 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, { Component } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 import SearchList from './screens/SearchList';
+import SongDetail from './screens/SongDetail';
 
-const App = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <SearchList />
-    </SafeAreaView>
-  );
+const NAVIGATION_SCREENS = {
+  SEARCH_LIST: 'SEARCH_LIST',
+  SONG_DETAIL: 'SONG_DETAIL',
 };
+
+export default class App extends Component {
+  state = {
+    currentScreen: NAVIGATION_SCREENS.SEARCH_LIST,
+    app: {
+      selectedTrack: null,
+    },
+  };
+
+  navigateToSongDetail = selectedTrack => {
+    this.setState({
+      currentScreen: NAVIGATION_SCREENS.SONG_DETAIL,
+      app: { selectedTrack },
+    });
+  };
+
+  navigateToSearchList = () => {
+    this.setState({
+      currentScreen: NAVIGATION_SCREENS.SEARCH_LIST,
+      app: { selectedTrack: null },
+    });
+  };
+
+  render() {
+    const {
+      currentScreen,
+      app: { selectedTrack },
+    } = this.state;
+
+    let screenComponent;
+    switch (currentScreen) {
+      case NAVIGATION_SCREENS.SEARCH_LIST:
+        screenComponent = (
+          <SearchList onPressSong={this.navigateToSongDetail} />
+        );
+        break;
+
+      case NAVIGATION_SCREENS.SONG_DETAIL:
+        screenComponent = (
+          <SongDetail
+            data={selectedTrack}
+            onPressBack={this.navigateToSearchList}
+          />
+        );
+        break;
+
+      default:
+        throw new Error(`Missing screen for ${currentScreen}`);
+    }
+
+    return (
+      <SafeAreaView style={styles.container}>{screenComponent}</SafeAreaView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
 });
-
-export default App;
