a262d10c5b1a440ba4f512ce29e568a7d76a971a
Kevin Nguy <nguy@macbook.local>
2020-05-20 15:29:58 -0700

    Implement SongList component

--- src/components/SongList/index.js
+++ src/components/SongList/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Flatlist, StyleSheet, View } from 'react-native';
+import React, { Component } from 'react';
+import { FlatList, StyleSheet, View } from 'react-native';
 
 import SongRow from '../SongRow';
 
-export default function SongList({ data }) {
-  return (
-    <Flatlist
-      data={data}
-      renderItem={({ item }) => <SongRow data={item} />}
-      keyExtractor={item => item.id}
-    />
-  );
+export default class SongList extends Component {
+  keyExtractor = item => item.trackId;
+
+  renderItem = ({ item }) => <SongRow data={item} />;
+
+  render() {
+    const { data } = this.props;
+    return (
+      <FlatList
+        style={{}}
+        data={data}
+        renderItem={this.renderItem}
+        keyExtractor={this.keyExtractor}
+      />
+    );
+  }
 }
