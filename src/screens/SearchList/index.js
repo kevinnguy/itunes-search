55d91b8e7f0d62d664e7851a5aae0327e62a8914
Kevin Nguy <nguy@macbook.local>
2020-05-20 16:06:45 -0700

    Implement searching feature in SearchList screen

--- src/screens/SearchList/index.js
+++ src/screens/SearchList/index.js
@@ -430,20 +430,34 @@ const fetchSongResults = async term => {
 export default class SearchList extends Component {
   state = {
     data: null,
+    searchQuery: '',
     isLoading: false,
   };
 
-  onChangeText = async text => {
+  onChangeText = searchQuery => {
+    this.setState({ searchQuery });
+  };
+
+  onPressSearch = async () => {
+    const { searchQuery } = this.state;
+    if (!searchQuery || !searchQuery.length) {
+      return;
+    }
+
     this.setState({ isLoading: true });
-    const data = await debounce(() => fetchSongResults(text), 300);
-    this.setState({ data, isLoading: false });
+    const results = await fetchSongResults(searchQuery);
+    this.setState({ data: results, isLoading: false });
   };
 
   render() {
     const { data, isLoading } = this.state;
     return (
       <View style={styles.container}>
-        <SearchBar onChangeText={this.onChangeText} isLoading={isLoading} />
+        <SearchBar
+          onChangeText={this.onChangeText}
+          isLoading={isLoading}
+          onPress={this.onPressSearch}
+        />
         <SongList data={data} />
       </View>
     );
