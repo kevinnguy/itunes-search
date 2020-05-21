d00bf2915a33a66b470ff6ede1a1f8259eef106c
Kevin Nguy <nguy@macbook.local>
2020-05-20 17:34:23 -0700

    Refactor layout in SongRow component

--- src/components/SongRow/index.js
+++ src/components/SongRow/index.js
@@ -1,48 +1,70 @@
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 
-export default function SongRow({ data }) {
-  const { artistName, artworkUrl100, trackName, collectionName } = data;
+export default function SongRow({ data, style, artworkStyle, showDetails }) {
+  const {
+    artistName,
+    artworkUrl100,
+    trackName,
+    collectionName,
+    trackPrice,
+    collectionPrice,
+    trackExplicitness,
+    primaryGenreName,
+  } = data;
+  const isExplicit = trackExplicitness === 'explicit';
+
   return (
-    <View style={styles.container}>
-      <Image style={styles.artwork} source={{ uri: artworkUrl100 }} />
-      <View style={styles.details}>
-        <Text style={styles.track} numberOfLines={1}>
-          {trackName}
+    <View style={[styles.container, style]}>
+      <Image
+        style={[styles.artwork, artworkStyle]}
+        source={{ uri: artworkUrl100 }}
+      />
+      <View style={[styles.details, !showDetails && { flex: 1 }]}>
+        <Text style={styles.description} numberOfLines={1}>
+          {showDetails ? `Track: ${trackName}` : trackName}
         </Text>
-        <Text style={styles.artist} numberOfLines={1}>
-          {artistName}
+        <Text style={styles.description} numberOfLines={1}>
+          {showDetails ? `Artist: ${artistName}` : artistName}
         </Text>
-        <Text style={styles.collection} numberOfLines={2}>
-          {collectionName}
+        <Text style={styles.description} numberOfLines={2}>
+          {showDetails ? `Album: ${collectionName}` : collectionName}
         </Text>
       </View>
+      {showDetails && (
+        <View style={styles.moreDetails}>
+          <Text style={styles.description}>{`$${trackPrice}`}</Text>
+          <Text
+            style={
+              styles.description
+            }>{`Buy the album for $${collectionPrice}`}</Text>
+          <Text style={styles.description}>
+            {`Genre: ${primaryGenreName}`}
+            {isExplicit && ' (Explicit)'}
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     flexDirection: 'row',
     height: 120,
     padding: 10,
   },
   artwork: {
     width: 100,
+    marginRight: 10,
   },
   details: {
-    flex: 1,
     justifyContent: 'space-between',
-    marginLeft: 10,
   },
-  artist: {
-    fontSize: 20,
+  moreDetails: {
+    marginTop: 20,
   },
-  track: {
-    fontSize: 20,
-  },
-  collection: {
+  description: {
     fontSize: 18,
   },
 });
