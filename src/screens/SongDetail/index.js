6abb01fab8129e339abd78357fc490d44ba114cc
Kevin Nguy <nguy@macbook.local>
2020-05-20 17:34:31 -0700

    Implement SongDetail screen

--- src/screens/SongDetail/index.js
+++ src/screens/SongDetail/index.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+} from 'react-native';
 
 import SongRow from '../../components/SongRow';
 
 export default function SongDetail({ data, onPressBack }) {
   return (
-    <View style={styles.container}>
-      <SongRow data={data} />
-    </View>
+    <ScrollView style={styles.container}>
+      <TouchableOpacity style={styles.backButton} onPress={onPressBack}>
+        <Text style={styles.backButtonText}>{'Back to search'}</Text>
+      </TouchableOpacity>
+      <SongRow
+        showDetails
+        style={styles.songRow}
+        artworkStyle={styles.artworkStyle}
+        data={data}
+      />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'brown',
+  },
+  backButton: {
+    borderColor: 'purple',
+    backgroundColor: 'purple',
+    borderRadius: 10,
+    borderWidth: StyleSheet.hairlineWidth,
+    padding: 10,
+    width: 120,
+    margin: 10,
+  },
+  backButtonText: {
+    color: 'white',
+  },
+  songRow: {
+    flexDirection: 'column',
+    // height: '100%',
+  },
+  artworkStyle: {
+    height: 200,
+    width: 200,
+    marginBottom: 10,
   },
 });
